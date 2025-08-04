8d443fa72a4467bb4402c4fef1e12a8a9766f3b4
Akryst <akryst.chr@gmail.com>
2025-08-04 16:49:35 -0700

    Remove call to markAsVisited on welcome screen initialization

--- public/js/modules/welcomeScreen.js
+++ public/js/modules/welcomeScreen.js
@@ -117,7 +117,6 @@ class WelcomeScreen {
 document.addEventListener('DOMContentLoaded', () => {
     if (WelcomeScreen.shouldShow()) {
         new WelcomeScreen();
-        WelcomeScreen.markAsVisited();
     }
 });
 
